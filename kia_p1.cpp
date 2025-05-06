c802762750a906ce6b8dcb421ea05ccabde5abe6
MHBD <bakhtiarim81@gmail.com>
2025-05-06 18:22:31 +0330

    tutorial started

--- kia_p1.cpp
+++ kia_p1.cpp
@@ -1,27 +1,67 @@
 #include<iostream>
-using namespace std;
+
+
+/*
+namespace
+*/
+// using namespace std;
+
+using std::cout;
+using std::string;
+
+namespace first{
+    int x = 1;
+}
+
+namespace second{
+    int x=2;
+}
+
+/*
+typedef and using
+*/
+
+typedef double BIT_32;
+using BIT_16 = int;
+
+
 
 int main(){
+
+
     // commment
     /*
     this is a
     multi line comment
     */
-    int num = 0;
-    cout << "enter a number: ";
-    cin >> num;
-    cout << "The Entered number : " <<num;
-    cout << "bug fixed";
-    cout << "cool feature";
-    cout << "bug fixed2";
-    cout << "a very new cool feature";
-    //
-    cout << "the main branch started";
-    cout << "the main branch continue2";
-    //
 
-    //
-    cout << "bug2 fixed";
-    cout << "bug2 fixed2";
-    return 0;
+    /*
+    variables
+    */
+    int num = 0;
+    double gpa = 12.5;
+    char cuurency = 'A';
+    string name = "ahmed";
+    cout << name << '\n';
+
+    /*
+    const (read only variables)
+    */
+   const double PI = 3.14; //connot be re-assigned
+
+   /*
+   namespace
+   */ 
+   int x = 0;
+   using namespace second;
+   using namespace first;
+   cout<< "x: "<< x<<'\n';
+
+/*
+typedef
+*/
+BIT_16 num2 = 12;
+cout<<"num2: "<< num2 <<'\n';
+
+   return 0;
 }
