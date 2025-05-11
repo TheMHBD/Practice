e0210c2deed144d8a6e120c4487c9fc81b512739
MHBD <bakhtiarim81@gmail.com>
2025-05-11 14:43:03 +0330

    until 1:47 in the video

--- kia_p1.cpp
+++ kia_p1.cpp
@@ -43,6 +43,7 @@ int main(){
     char cuurency = 'A';
     string name = "ahmed";
     cout << name << '\n';
+    
 
     /*
     const (read only variables)
@@ -61,7 +62,52 @@ int main(){
 typedef
 */
 BIT_16 num2 = 12;
+
+num2 += 2;
 cout<<"num2: "<< num2 <<'\n';
+
+// if satement
+if(num2>=20){
+    cout<<"greater than 20";
+}
+else if(num2<=10){
+    cout<<"smaller than 10";
+}
+else{
+    cout<<"between 10 and 20 \n";
+}
+
+switch (num2%2)
+{
+case 1:
+    cout<<"num2 is an odd number\n";
+    break;
+case 0:
+    cout<<"num2 is an even number\n";
+    break;
+default:
+    break;
+}
+
+string name2 = "Mohammad";
+
+if (!name2.empty()){
+    cout<<"hello "<<name2<<'\n';
+    cout<< "your name length: "<<name2.length()<<'\n';
+    cout<< "your user name is: "<<name2.append("@gmail.com")<<'\n';
+    cout<< "your username last charachter: "<<name2.at(name2.length()-1)<<'\n';
+
+
+}
+
+for(int i=1;i<=10;i=i+1){
+    cout<<"i: "<<i<<'\n';
+    if (i==5){
+        continue;
+    }
+}
+
 
    return 0;
 }
+
